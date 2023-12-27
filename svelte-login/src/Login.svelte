f51224150c84051a7ffc3d0d8e594a0d3ef46b96
shirako <41273764+shirakoisnull@users.noreply.github.com>
2023-12-28 01:52:52 +0200

    feat: add login component

---
+++ svelte-login/src/Login.svelte
@@ -0,0 +1,23 @@
+<script>
+  let username = '';
+  let password = '';
+
+  function handleSubmit() {
+    // Handle form submission (validate, authenticate, etc.)
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Implement your logic to authenticate here
+  }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <label>
+    Username:
+    <input type="text" bind:value={username} />
+  </label>
+  <label>
+    Password:
+    <input type="password" bind:value={password} />
+  </label>
+  <button type="submit">Login</button>
+</form>
