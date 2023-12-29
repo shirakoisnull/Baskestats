40e739f42d055e2ff9f8de3dbd09adcff3fcee20
shirako <41273764+shirakoisnull@users.noreply.github.com>
2023-12-29 14:04:13 +0200

    fix: proper vite + svelte project

---
+++ app/client/baskestats/src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Login from "./Login.svelte";
+</script>
+
+<h1>Baskestats</h1>
+<main>
+  <div class="card">
+    <Login />
+  </div>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  /* @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  } */
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+</style>
