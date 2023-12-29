6b590bdcfdc07c2759880710fef481e4e5ee5733
shirako <41273764+shirakoisnull@users.noreply.github.com>
2023-12-29 19:15:01 +0200

    fix: add CORS error fix to login.py

---
+++ app/backend/login.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, url_for, redirect, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+import pymysql
+import os
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+
+#Install requirements with pip install --upgrade -r requirements.txt
+
+app = Flask(__name__)
+CORS(app)
+
+load_dotenv()
+
+db_host = os.environ['DB_HOST']
+db_user = os.environ['DB_USER']
+db_password = os.environ['DB_PASSWORD']
+db_name=os.environ['DB_NAME']
+
+app.config["JWT_SECRET_KEY"] = os.environ['JWT_SECRET_KEY']
+jwt = JWTManager(app)
+
+db = pymysql.connect(host=db_host, user=db_user, password=db_password, database=db_name)
+
+@app.route('/login',methods=['GET'])
+def getLogin():
+    cursor = db.cursor()
+    cursor.execute('''
+                    SELECT * FROM secretary
+                   ''')
+    results=cursor.fetchall()
+
+    cursor.close
+    return jsonify(results)
+
+
+# Logging in
+@app.route('/login',methods=['POST'])
+def login():
+        # Gets the username and password from the input form of the frontend
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        # Making a cursor/pointer object for interacting with the database
+        cursor = db.cursor()
+        # Executing the query using parametirised variables using the %s placeholder for preventing SQL injections
+        cursor.execute('SELECT password FROM secretary WHERE username = %s',(username,))
+        # Fetching one instance
+        result=cursor.fetchone()
+        cursor.close
+        # Checking if result exists and is equal to my password
+        if result and result[0] == password:
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token, username)
+        else:
+            return 'Error'
+
+#Gettng current user
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+     current = get_jwt_identity()
+     return jsonify(user=current), 200
+
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
