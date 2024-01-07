77cd8bc788c55f221aa145d88be7f98a7a49fb86
shirako <41273764+shirakoisnull@users.noreply.github.com>
2024-01-07 23:15:23 +0200

    fix: player team name displayed correctly

--- app/client/baskestats/src/modals/PlayerModal.svelte
+++ app/client/baskestats/src/modals/PlayerModal.svelte
@@ -3,7 +3,17 @@
   export let results = [];
   export let closeModal = () => {};
 
-  console.log(results);
+  import { viewTeam } from '../api';
+  import { onMount } from 'svelte';
+  
+  let teamInfo = [];
+  let teamName = "";
+
+  onMount(async () => {
+    teamInfo = await viewTeam(results[1]);
+    teamName = teamInfo[1];
+  });
+
 </script>
 
 {#if showModal}
@@ -29,7 +39,7 @@
         </thead>
         <tbody>
             <tr>
-              <td>{results[1]}</td>
+              <td>{teamName}</td>
               <td>{results[3]}</td>
               <td>{results[4]} cm</td>
               <td>{results[5]} kg</td>
